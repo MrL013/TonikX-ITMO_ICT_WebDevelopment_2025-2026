58bdf3e8103abd85d98ef237fd2120c7fbae39c3
MrL013 <lev.rock@yandex.ru>
2025-12-17 17:16:59 +0300

    fix: redirect login and logout

--- students/k3340/Sinyukov_Lev/Lr_2/confsite/confsite/urls.py
+++ students/k3340/Sinyukov_Lev/Lr_2/confsite/confsite/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page=reverse_lazy('conf_list')), name='logout'),
+    path('accounts/profile/', RedirectView.as_view(url=reverse_lazy('conf_list'))),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('conferences.urls')),
 ]
